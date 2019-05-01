0d860e38679cba0ddbebbbff153bbc8449937d19
Yung Dai <yungchidai@gmail.com>
2019-05-01 09:40:30 -0400

    Initial Commit

---
+++ AppStoreAnimationDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreAnimationDemo
+//
+//  Created by Yung Dai on 2019-05-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ AppStoreAnimationDemoTests/AppStoreAnimationDemoTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppStoreAnimationDemoTests.swift
+//  AppStoreAnimationDemoTests
+//
+//  Created by Yung Dai on 2019-05-01.
+//
+
+import XCTest
+@testable import AppStoreAnimationDemo
+
+class AppStoreAnimationDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
